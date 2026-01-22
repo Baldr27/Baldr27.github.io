af2f82cff2ed4b1ab973647d17fbe86d69161c2c
Baldr27 <107594029+Baldr27@users.noreply.github.com>
2026-01-22 01:05:11 +0000

    Deploying to gh-pages from @ Baldr27/Baldr27.github.io@577e56f3a277262b9106181c718f3f6a308eae79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,51 +16,31 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+        },{id: "projects-gash-dataset-y-gather",
+          title: 'GASH Dataset y GATHER',
+          description: "Tesis/Proyecto de Graduación - Generación de datasets de vulnerabilidades",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/1_gash_gather/";
+            },},{id: "projects-bikingo",
+          title: 'bikinGO',
+          description: "Proyecto de Universidad - App de trazado de rutas para ciclistas",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/2_bikingo/";
+            },},{id: "projects-gravat",
+          title: 'Gravat',
+          description: "Desarrollador Front-End Web",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/3_gravat/";
+            },},{id: "projects-grupo-giecid",
+          title: 'Grupo GIECID',
+          description: "Desarrollo Web para grupo de investigación",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/4_giecid/";
+            },},{id: "projects-redopa",
+          title: 'redOPA',
+          description: "Práctica 1 - Chatbot en WordPress",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/5_redopa/";
             },},{
         id: 'social-cv',
         title: 'CV',
